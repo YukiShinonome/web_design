6f7e121601fcaa7535869200a439b1b6fa024836
YukiShinonome <yuki.yamakoshi0126@gmail.com>
2020-08-23 00:08:13 +0900

    メニューボタンとメイン画像の表示

--- pages/cafe-01.vue
+++ pages/cafe-01.vue
@@ -1,3 +1,22 @@
 <template>
-  <div>Cafe</div>
+  <div>
+    <v-tabs grow background-color="transparent" color="#000" slider-color="#8b4513">
+      <v-tab>home</v-tab>
+      <v-tab>menu</v-tab>
+      <v-tab>contact</v-tab>
+    </v-tabs>
+    <v-img
+      src="https://cdn.pixabay.com/photo/2016/02/03/17/29/coffee-1177533_960_720.jpg"
+      max-height="500px"
+    ></v-img>
+  </div>
 </template>
+
+<script lang="ts">
+</script>
+
+<style lang="scss" scoped>
+.v-tabs {
+  margin-bottom: 10px;
+}
+</style>
