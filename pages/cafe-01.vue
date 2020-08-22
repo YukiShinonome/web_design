f3e116a8c9086041120ba1bd5a1a2c47d7a5f8fb
YukiShinonome <yuki.yamakoshi0126@gmail.com>
2020-08-23 01:28:48 +0900

    ニュースコンポーネントの切り出し

--- pages/cafe-01.vue
+++ pages/cafe-01.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="container">
-    <v-tabs grow background-color="transparent" color="#000" slider-color="#8b4513">
+    <v-tabs
+      grow
+      background-color="transparent"
+      color="#000"
+      slider-color="#8b4513"
+    >
       <v-tab>home</v-tab>
       <v-tab>menu</v-tab>
       <v-tab>access</v-tab>
@@ -10,66 +15,18 @@
       src="https://cdn.pixabay.com/photo/2016/02/03/17/29/coffee-1177533_960_720.jpg"
       max-height="500px"
     ></v-img>
-    <div class="news-title text-center">NEWS</div>
-    <v-simple-table class="table-margin" height="300px">
-      <tbody>
-        <tr v-for="(item, idx) in items" :key="idx">
-          <td>{{ item.category }}</td>
-          <td>{{ item.content }}</td>
-        </tr>
-      </tbody>
-    </v-simple-table>
+    <news></news>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import News from '~/components/cafe-01/News.vue'
 
 export default Vue.extend({
-  data: () => ({
-    items: [
-      {
-        category: 'お知らせ',
-        content:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'
-      },
-      {
-        category: 'お知らせ',
-        content:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'
-      },
-      {
-        category: 'メニュー',
-        content:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'
-      },
-      {
-        category: 'お知らせ',
-        content:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'
-      },
-      {
-        category: 'メニュー',
-        content:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'
-      },
-      {
-        category: 'お知らせ',
-        content:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'
-      },
-      {
-        category: 'お知らせ',
-        content:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'
-      },
-      {
-        category: 'メニュー',
-        content:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'
-      }
-    ]
-  })
+  components: {
+    News
+  }
 })
 </script>
 
@@ -84,17 +41,5 @@ export default Vue.extend({
 
 .v-tab {
   font-size: 1.2rem;
-}
-
-.news-title {
-  font-size: 1.2rem;
-  padding: 20px 0 10px 0;
-  margin: 0 20px;
-  border-bottom: 2px solid #8b4513;
-}
-
-.theme--light.v-data-table {
-  background-color: transparent;
-  margin: 0 20px;
 }
 </style>
