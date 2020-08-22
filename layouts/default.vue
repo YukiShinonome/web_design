590302c2afabf582ec14c80331d8ad345debaa51
YukiShinonome <yuki.yamakoshi0126@gmail.com>
2020-08-22 17:50:46 +0900

    レイアウトのベースを作成

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,8 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
+  <v-app>
+    <v-navigation-drawer app clipped>
       <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -24,94 +12,60 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+
+    <!-- <v-app-bar app clipped-left flat>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-btn text nuxt to="/">
+        <v-toolbar-title v-text="title" />
       </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
+    </v-app-bar>-->
+
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+
+    <!-- <v-footer app>
+      <span>&copy; 2020</span>
+    </v-footer>-->
   </v-app>
 </template>
 
 <script>
 export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  }
+  props: {
+    source: String
+  },
+
+  data: () => ({
+    drawer: false,
+    items: [
+      {
+        icon: 'mdi-apps',
+        title: 'Welcome',
+        to: '/'
+      },
+      {
+        icon: 'mdi-cog',
+        title: '設定',
+        to: '/settings'
+      }
+    ],
+    title: 'Web Design'
+  })
 }
 </script>
+
+<style lang="scss" scoped>
+.v-btn {
+  text-transform: none;
+
+  &::before {
+    background-color: transparent;
+  }
+}
+.theme--light.v-app-bar.v-toolbar.v-sheet {
+  background-color: azure;
+}
+</style>
---
+++ pages/settings.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card outlined>
+    <v-card-title primary-title class="no_padding_bottom">設定</v-card-title>
+    <v-card-text>
+      <v-container fluid class="no_padding_top">
+        <v-row>
+          <v-col cols="12" sm="4" md="4">
+            <v-switch label="ダークモード" v-model="$vuetify.theme.dark" hide-details></v-switch>
+            <v-switch label="＊＊＊＊＊" hide-details></v-switch>
+          </v-col>
+          <v-col cols="12" sm="4" md="4">
+            <v-switch label="indigo" color="indigo" value="indigo" hide-details></v-switch>
+            <v-switch label="red" color="red" value="red" hide-details></v-switch>
+          </v-col>
+          <v-col cols="12" sm="4" md="4">
+            <v-switch label="orange" color="orange" value="orange" hide-details></v-switch>
+            <v-switch label="green" color="green" value="green" hide-details></v-switch>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss" scoped>
+.no_padding_top {
+  padding-top: 0;
+}
+.no_padding_bottom {
+  padding-bottom: 0;
+}
+</style>
